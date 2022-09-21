972fe2d0e5033d823cf19a279f968d46ef0acc55
Александр Бабаскин <babaskin.alexandr@gmail.com>
2022-09-21 11:16:33 +0200

    Добавьте файлы проекта.

---
+++ Humans/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+string[] names = GetLines();
+Print(names);
+
+/*string[] array = new string[1] { "" };
+Console.WriteLine("{0},{1},{2},{3},{4}",array[1], array[2], array[3], array[4], array[5]);
+*/
+string[] GetLines()
+{
+    string text = File.ReadAllText("D://russian_surnames.csv");
+    string[] names = text.Split('\n');
+    return names;
+}
+
+void Print(string[] lines)
+{
+    for (int i = 1; i < lines.Length; i++)
+    {
+            string[] arrays = lines[i].Split(';');
+        if (arrays.Length == 6)
+            Console.WriteLine($"{arrays[1]}");
+    }
+}
